34dd52cb04212ce704ad385b86171f75c27915fa
XGerman009 <52032203+XGerman009@users.noreply.github.com>
2022-01-26 18:19:56 +1200

    Add files via upload

---
+++ Epic Gui.lua
@@ -0,0 +1,12 @@
+loadstring(game:HttpGet("https://pastebin.com/raw/eCCmuezM", true))()
+--[[
+Key Functions:
+Z - Open/Close ApocX
+C - Enable/Disable Part Collision
+E - Lock Aim (When Aimbot is turned on)
+Platform Functions:
+K - Make Platform Appear
+L - Make Platform Dissapear
+CTRL LEFT - Make Platform move upwards
+CTRL RIGHT - Make Platform move downwards
+]]
